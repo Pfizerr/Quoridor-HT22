eb5873dab2ab6663d73c1c16b186cce9afe7cccb
pfizerr <45396385+Pfizerr@users.noreply.github.com>
2022-10-20 13:50:10 +0200

    fixes, now sorta working for one wall.

--- QgameProj/Student/AdjacencyList.cs
+++ QgameProj/Student/AdjacencyList.cs
@@ -4,7 +4,7 @@ using Microsoft.Xna.Framework;
 
 namespace Student
 {
-    
+
     public class AdjacencyList : Graph
     {
         private List<int>[] adjacencyList; // Space: E+V
@@ -29,7 +29,7 @@ namespace Student
         // O(N^2)
         public override void Build(SpelBräde bräde)
         {
-            bool[,] horizontalWalls = bräde.horisontellaVäggar;
+            bool[,] horizontalWalls = bräde.horisontellaVäggar; // Kan bara fylla 8 utav 9 platser på en rad/kolumn med väggar. (?)
             bool[,] verticalWalls = bräde.vertikalaVäggar;
             int N = SpelBräde.N;
 
@@ -39,25 +39,25 @@ namespace Student
                 {
                     int center = y * N + x;
 
-                    if (horizontalWalls[x, y])
+                    if (y < horizontalWalls.GetLength(1) - 1 && horizontalWalls[x, y])
                     {
                         if (ContainsEdge(center, center + 1))
                             RemoveEdge(center, center + 1);
                     }
                     else
                     {
-                        if (!ContainsEdge(center, center + 1))
+                        if ((center + 1) % N != 0 && !ContainsEdge(center, center + 1))
                             AddEdge(center, center + 1);
                     }
 
-                    if (verticalWalls[x, y])
+                    if (x < verticalWalls.GetLength(0) - 1 && verticalWalls[x, y])
                     {
                         if (ContainsEdge(center, center + N))
                             RemoveEdge(center, center + N);
                     }
                     else
                     {
-                        if (!ContainsEdge(center, center + N))
+                        if ((center + N < N * N) && !ContainsEdge(center, center + N))
                             AddEdge(center, center + N);
                     }
                 }
@@ -103,6 +103,162 @@ namespace Student
         public override IEnumerator<int> AdjacentTo(int v)
         {
             return adjacencyList[v].GetEnumerator();
+        }
+    }
+}
+
+
+namespace test
+{
+    public class Node
+    {
+        private static int N = SpelBräde.N;
+        private List<Node> adjacentNodes;
+        private Point location;
+
+        public Point Location
+        {
+            get
+            {
+                return location;
+            }
+            private set
+            {
+                location = value;
+            }
+        }
+
+        public int Index
+        {
+            get 
+            { 
+                return Location.Y * N + Location.X; 
+            }
+            private set 
+            { 
+                location = new Point(value % N, (value - value % N) / N);
+            }
+        }
+
+        public Node(Point location)
+        {
+            this.location = location;
+            adjacentNodes = new List<Node>();
+        }
+
+        public void AddEdge(Node other)
+        {
+            adjacentNodes.Add(other);
+        }
+
+        public void RemoveEdge(Node other)
+        {
+            adjacentNodes.Remove(other);
+        }
+
+        public void RemoveAllEdges()
+        {
+            for (int i = adjacentNodes.Count - 1; i >= 0; i--)
+            {
+                Node node = adjacentNodes[i];
+                node.RemoveEdge(this);
+                adjacentNodes.RemoveAt(i);
+            }
+        }
+
+        public IEnumerator<Node> AdjacentNodes()
+        {
+            return adjacentNodes.GetEnumerator();
+        }
+
+        public bool AdjacentTo(Node other)
+        {
+            bool result = false;
+
+            foreach(Node node in adjacentNodes)
+                if (node == other)
+                    result = true;
+
+            return result;
+        }
+    }
+
+    public class Graph
+    {
+        int N;//
+
+        private Node[] nodes;
+
+        public Graph(SpelBräde bräde)
+        {
+            nodes = new Node[N * N];
+            Build(bräde);
+        }
+
+        public void Build(SpelBräde bräde)
+        {
+            bool[,] horizontalWalls = bräde.horisontellaVäggar; // Kan bara fylla 8 utav 9 platser på en rad/kolumn med väggar. (?)
+            bool[,] verticalWalls = bräde.vertikalaVäggar;
+            int N = SpelBräde.N;
+
+            for (int y = 0; y < N; y++)
+            {
+                for (int x = 0; x < N; x++)
+                {
+                    int center = y * N + x;
+
+                    if (x < horizontalWalls.GetLength(1) && horizontalWalls[x, y])
+                    {
+                        if (ContainsEdge(center, center + 1))
+                            RemoveEdge(center, center + 1);
+                    }
+                    else
+                    {
+                        if (!ContainsEdge(center, center + 1))
+                            AddEdge(center, center + 1);
+                    }
+
+                    if (y < verticalWalls.GetLength(0) && verticalWalls[x, y])
+                    {
+                        if (ContainsEdge(center, center + N))
+                            RemoveEdge(center, center + N);
+                    }
+                    else
+                    {
+                        if (!ContainsEdge(center, center + N))
+                            AddEdge(center, center + N);
+                    }
+                }
+            }
+        }
+
+        public void AddEdge(int v, int w)
+        {
+            nodes[v].AddEdge(nodes[w]);
+            nodes[w].AddEdge(nodes[v]);
+        }
+
+        public void RemoveEdge(int v, int w)
+        {
+            nodes[v].RemoveEdge(nodes[w]);
+            nodes[w].RemoveEdge(nodes[v]);
+        }
+
+        public void RemoveAllEdges(int v)
+        {
+            IEnumerator<Node> enumerator = AdjacentTo(v);
+            while (enumerator.MoveNext())
+                RemoveEdge(v, enumerator.Current.Index);
+        }
+
+        public bool ContainsEdge(int v, int w)
+        {
+            return nodes[v].AdjacentTo(nodes[w]);
+        }
+
+        public IEnumerator<Node> AdjacentTo(int v)
+        {
+            return nodes[v].AdjacentNodes();
         }
     }
 }
