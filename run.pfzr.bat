cbcb7230139e160704f426d495b72f3ac75e14f3
pfizerr <45396385+Pfizerr@users.noreply.github.com>
2022-11-21 09:33:31 +0100

    before commets

--- run.pfzr.bat
+++ run.pfzr.bat
@@ -1,5 +1,8 @@
 @echo off
 start %USERPROFILE%\source\repos\Quoridor-HT22\Server\QGame.exe
 start %USERPROFILE%\source\repos\Quoridor-HT22\AISimple\QGame.exe
-start %USERPROFILE%\source\repos\Quoridor-HT22\QgameProj\Student\bin\debug\netcoreapp3.1\Student.exe
+start %USERPROFILE%\source\repos\Quoridor-HT22\Manual\QGame.exe
+
+rem start %USERPROFILE%\source\repos\Quoridor-HT22\QgameProj\Student\bin\debug\netcoreapp3.1\Student.exe
+
 exit
